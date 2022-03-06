3687b0ef5f746b6179aa947f49bec21215c27e96
CarlBush <Carl.BushS13@gmail.com>
2022-03-06 09:37:53 -0700

    stored to local storage(not retrieved)

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var currentHour = moment().format("HH");
-var hour =[12,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+var hour =[12,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
 //1st attempt fail var hour =["12AM", "1AM", "2AM", "3AM", "4AM","5AM", "6AM", "7AM","8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 
@@ -8,50 +8,50 @@ document.getElementById("currentDay").textContent = moment().format("dddd, MMMM 
 
 $(document).ready(function (){
 for (var i = 7; i < hour.length; i++){
+    //MAIN CONTAINER THAT WILL MERGE THE OTHER THREE CONTAINERS
     var combineThreeContainer = $("<div>").addClass("time-block row");
 
     //HOUR CONTAINER (1ST COLUMN)
     var timeContainer = $("<div>").addClass("hour col-1");
     timeContainer.text(hour[i] + ":00");
     
-    //DESCRIPTION CONTAINER (2ND MAIN-COLUMN)
-    var taskContainer = $("<div>").addClass("description col-10");
-
-    //DESCRIPTION TEXT-AREA CONTAINER (2ND SUB-COLUMN)
-    var descriptionContainer = $("<textarea>");
-    descriptionContainer.attr("id", [i]);
-    descriptionContainer.text('MEOOOOOOOOOOOOOOOOOOOOOOOOW');
-    taskContainer.append(descriptionContainer);
+    //DESCRIPTION CONTAINER TEXT-AREA (2ND COLUMN)
+    var descriptionContainer = $("<textarea>").addClass("description col-10 align-items-center");;
+    descriptionContainer.attr("id", "description" + [i]);
 
     //SAVE CONTAINER (3RD COLUMN)
-    var saveIcon =$("<i>").addClass("fas fa-save");
-    var saveContainer = $("<div>").addClass("saveBtn col-1");
+    var saveIcon =$("<i>").addClass("fas fa-save").attr("id", "save");
+    var saveContainer = $("<div>").addClass("saveBtn col-1 align-items-center");
     saveContainer.attr("id", [i]);
     saveContainer.append(saveIcon);
 
     //CONDITIONAL FORMATTING
     if(currentHour == hour[i]){
         descriptionContainer.addClass("present")
-        console.log(hour[i]);
-        console.log(currentHour);
     }
     else if(currentHour > hour[i]){
         descriptionContainer.addClass("past")
-        console.log(hour[i]);
-        console.log(currentHour);
     }
     else{
         descriptionContainer.addClass("future")
-        console.log(hour[i]);
-        console.log(currentHour);
-    }
-    ;
+    };
 
-
-    $(combineThreeContainer).append(timeContainer, taskContainer, saveContainer);
+    //APPENDING THE THREE CONTAINERS TO MAIN CONTAINER THEN TO CLASS .container
+    $(combineThreeContainer).append(timeContainer, descriptionContainer, saveContainer);
     $(".container").append(combineThreeContainer);
-    
-
 };
-console.log(currentHour);
 });
+
+
+
+
+$(document).ready(function(){
+    $(".saveBtn").on("click", function() {
+        var descriptionTextAreaId = "#description" + (this.id);
+        var descriptionInput = document.querySelector(descriptionTextAreaId);
+        localStorage.setItem(descriptionTextAreaId, descriptionInput.value);
+        console.log(descriptionTextAreaId);
+        console.log(descriptionInput);
+    });
+});
+
